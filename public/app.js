244cdd36d044225caf4be90f5b99904533ecd75b
2021147585 <greenet503@yonsei.ac.kr>
2022-06-08 17:54:10 +0900

    html, css, script, img의 내용물이 같은 폴더에 있다고 생각하고 코드 작성 바람

---
+++ public/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/html/mainpage.html");
+});
+
+app.use(express.static(__dirname + "/css"));
+app.use(express.static(__dirname + "/html"));
+app.use(express.static(__dirname + "/img"));
+app.use(express.static(__dirname + "/script"));
+
+app.listen(3000, () =>{
+    console.log("now listening on port 3000");
+});
